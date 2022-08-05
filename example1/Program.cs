935771ba53786ea3d15f21672359219dca624707
niyaz <xamidyll@gmail.com>
2022-08-05 12:39:21 +0300

    defefe

--- example1/Program.cs
+++ example1/Program.cs
@@ -1,7 +1,43 @@
 ﻿Console.Clear();
 
-int xa = 1, ya = 1,
-     xb = 30, yb = 30,
-    xc = 80, yc = 80;
-Console.SetCursorPosition(xa,ya);
+int xa = 40, ya = 1,
+    xb = 1, yb = 20,
+    xc = 80, yc = 20;
+Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = ya;
+
+int count = 0;
+
+ while (count < 1000d)
+{
+    int what = new Random().Next(0, 3);
+
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+            y = (y + ya) / 2;
+        }
+
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+            y = (y + yb) / 2;
+        }
+
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+            y = (y + yc) / 2;
+        }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count = count + 1;
+}
